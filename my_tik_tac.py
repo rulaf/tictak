00436f23b28ced7b4b05420a03a8ee2e1389b643
Ruald <ruald@gmail.com>
2022-11-12 10:02:25 +0300

    correcting  and commenting algoritm

--- my_tik_tac.py
+++ my_tik_tac.py
@@ -2,6 +2,7 @@ import random
 
 
 def print_hello():
+    # Приветствие
     print('Игра в крестики нолики с компьютером!')
     print('Вы играете за крестики - Х')
     print('Чтобы сделать ход введите число от 1 до 9   ')
@@ -28,6 +29,7 @@ winner = [
 
 
 def gaming_table():
+    # Печать текушей игровой таблицы и шаблона
     print(f'''                    ИГРА            ШАБЛОН
                  {table_temp[0]} | {table_temp[1]} | {table_temp[2]}      | 1 | 2 | 3 |
                  {table_temp[3]} | {table_temp[4]} | {table_temp[5]}      | 4 | 5 | 6 |
@@ -36,7 +38,7 @@ def gaming_table():
 
 
 def gaming_step():
-    '''Ход игрока.'''
+    # Ход игрока
     key = input(f'Введите число от 1 до 9 в соответствии с шаблоном  в правой таблице "X": ')
     while key not in map(str, ([i for i in range(1, 10)])) or table_temp[int(key) - 1] != '-':
         key = input(f'Ставить крестик только в свободное место и вводить числа только от 1 до 9: ')
@@ -45,7 +47,7 @@ def gaming_step():
     print('После вашего хода')
     gaming_table()
     win_detect()
-    '''Ход бота.'''
+    # Ход бота
     if '-' not in table_temp:
         print('Ничья')
         exit()
@@ -59,6 +61,7 @@ def gaming_step():
 
 
 def win_detect():
+    # Определение победителя
     for i in winner:
         if table_temp[i[0]] == table_temp[i[1]] == table_temp[i[2]] == 'X':
             print('Вы победили, поздравляем')
@@ -68,11 +71,11 @@ def win_detect():
             exit()
 
 
-# Игра
 def main():
-    gaming_table()
     while True:
-       gaming_step()
+        gaming_step()
+
 
 print_hello()
+gaming_table()
 main()
