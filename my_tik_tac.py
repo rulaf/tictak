7dde0e56e71245737065f8d1b2e44f017ed07fd1
Ruald <ruald@gmail.com>
2022-11-12 09:28:07 +0300

    correcting algoritm

--- my_tik_tac.py
+++ my_tik_tac.py
@@ -2,7 +2,8 @@ import random
 
 
 def print_hello():
-    print('Игра в крестики нолики с ботом!')
+    print('Игра в крестики нолики с компьютером!')
+    print('Вы играете за крестики - Х')
     print('Чтобы сделать ход введите число от 1 до 9   ')
     print('в соответствии с шаблоном  в правой таблице ')
 
@@ -38,14 +39,23 @@ def gaming_step():
     '''Ход игрока.'''
     key = input(f'Введите число от 1 до 9 в соответствии с шаблоном  в правой таблице "X": ')
     while key not in map(str, ([i for i in range(1, 10)])) or table_temp[int(key) - 1] != '-':
-        key = input(f'Ставить только в пустое место и только от 1 до 9: ')
+        key = input(f'Ставить крестик только в свободное место и вводить числа только от 1 до 9: ')
     key = int(key)
     table_temp[key - 1] = 'X'
+    print('После вашего хода')
+    gaming_table()
+    win_detect()
     '''Ход бота.'''
+    if '-' not in table_temp:
+        print('Ничья')
+        exit()
     key_bot = random.randint(1, 9)
     while table_temp[int(key_bot) - 1] != '-':
         key_bot = random.randint(1, 9)
     table_temp[key_bot - 1] = '0'
+    print('После хода компьютера')
+    gaming_table()
+    win_detect()
 
 
 def win_detect():
@@ -55,19 +65,14 @@ def win_detect():
             exit()
         if table_temp[i[0]] == table_temp[i[1]] == table_temp[i[2]] == '0':
             print('Победил компьютер, не расстраивайтесь')
+            exit()
 
 
-# Алгоритм игры
+# Игра
 def main():
-    counter = 0
+    gaming_table()
     while True:
-        counter += 1
-        gaming_table()
-        gaming_step()
-        win_detect()
+       gaming_step()
 
-        if counter == 9:
-            print('Ничья')
-
-
+print_hello()
 main()
